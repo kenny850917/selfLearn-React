bfd821b5ece4b2c8ece52ff64205a40edc53e9e2
kenny850917 <kenny850917@users.noreply.github.com>
2020-02-10 23:22:50 -0800

    Destructuring argument
    destructure counters class to app class

--- counter/src/components/counter.jsx
+++ counter/src/components/counter.jsx
@@ -1,65 +1,65 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.value
-    // tags: ["tag1", "tag2", "tag3"]
-  };
+  // state = {
+  //   value: this.props.value
+  //   // tags: ["tag1", "tag2", "tag3"]
+  // };
 
   // styles={
   //     fontSize: 50,
   //     fontWeight: 'bold'
   // };
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  // renderTags() {
+  //   if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+  //   return (
+  //     <ul>
+  //       {this.state.tags.map(tag => (
+  //         <li key={tag}>{tag}</li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
   //this is the basic way
   // constsructor() {
   //   super();
   //   this.handleIncrement = this.handleIncrement.bind(this);
   // }
 
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
+  // handleIncrement = () => {
+  //   this.setState({ value: this.state.value + 1 });
+  // };
 
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={()=>this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
         </button>
-        <div></div>;
+        <div></div>
       </React.Fragment>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value: count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
--- counter/src/components/counters.jsx
+++ counter/src/components/counters.jsx
@@ -1,43 +1,27 @@
 import React, { Component } from "react";
 import Counter from "./counter";
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
-
-  handleReset =()=>{
-      const counters = this.state.counters.map(c=>{
-        c.value=0;
-        return c;
-      });
-      this.setState({counters});
-  };
-
-  handleDelete = counterId => {
-    // console.log("delete clicked", counterId);
-    const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({ counters: counters });
-  };
+  
   render() {
+    const{onReset, counters, onDelete, onIncrement} = this.props;
+
+
     return (
       <div>
-          
-          <button onClick = {this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
-        {this.props.children}
-        {this.state.counters.map(counter => (
+        <button
+          onClick={onReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+       
+        {counters.map(counter => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
-            value={counter.value}
-            id={counter.id}
-          >
-            {/* <h4>Counter #{counter.id} </h4> */}
-          </Counter>
+            counter={counter}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+          ></Counter>
         ))}
       </div>
     );
