faa84877846b6e19ac07759722ba8058cb44fd4e
kenny850917 <kenny850917@users.noreply.github.com>
2020-02-11 15:04:52 -0800

    nav bar counters

--- counter/src/App.js
+++ counter/src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <NavBar totalCounter={this.state.counters.filter(c=>c.value>0).length} />
+        <NavBar totalCounters={this.state.counters.filter(c=>c.value>0).length} />
         <main className="container">
           <Counters
             counters={this.state.counters}
--- counter/src/components/NavBar.jsx
+++ counter/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 
+//stateless functional component
 const NavBar = ({totalCounters}) => {
   return (
     <nav className="navbar navbar-light bg-light">
